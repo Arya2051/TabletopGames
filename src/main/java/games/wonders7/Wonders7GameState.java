e684c6c17f04d8e34f4114933c3cbae5caaee83b
awais <aryakakaroz@hotmail.com>
2023-04-30 01:10:40 +0100

    Altered copy() in gamestate for playerWonderBoard and turnActions Added hashcode function in gamestate.

--- src/main/java/games/wonders7/Wonders7GameState.java
+++ src/main/java/games/wonders7/Wonders7GameState.java
@@ -36,26 +36,28 @@ public class Wonders7GameState extends AbstractGameState {
 
         // Each player starts off with no resources
         playerResources = new ArrayList<>(); // New arraylist , containing different hashmaps for each player
-        for (int i=0; i<getNPlayers(); i++){
+        for (int i = 0; i < getNPlayers(); i++) {
             playerResources.add(new HashMap<>());
 
         }
 
         // Then fills every player's hashmaps, so each player has 0 of each resource
-        for (int i=0; i<getNPlayers(); i++){ // For each
-            for (Wonders7Constants.resources type: Wonders7Constants.resources.values()){playerResources.get(i).put(type, 0);}
+        for (int i = 0; i < getNPlayers(); i++) { // For each
+            for (Wonders7Constants.resources type : Wonders7Constants.resources.values()) {
+                playerResources.get(i).put(type, 0);
+            }
         }
 
 
     }
 
-    public List<Component> _getAllComponents(){
+    public List<Component> _getAllComponents() {
         // return a list of all (parents, and those nested as well) components in your game state. The method is called after game setup, so you may assume all components are already created. Decks and Areas have all of their nested components automatically added.
 
         return new ArrayList<>();
     }
 
-    public AbstractGameState _copy(int playerId){
+    public AbstractGameState _copy(int playerId) {
         // define a reduced, player-specific, copy of your game state
         // Including components that player with the given ID will see.
         // For example, some decks may be face down and unobservable to the player
@@ -64,28 +66,33 @@ public class Wonders7GameState extends AbstractGameState {
         copy.playerResources = new ArrayList<>();
         copy.playerHands = new ArrayList<>();
         copy.playedCards = new ArrayList<>();
-        copy.playerWonderBoard = new Wonder7Board[copy.getNPlayers()];
+        copy.playerWonderBoard = new Wonder7Board[getNPlayers()];
+        copy.turnActions = new AbstractAction[getNPlayers()];
 
-        for (HashMap<Wonders7Constants.resources, Integer> map: playerResources) {
+        for (HashMap<Wonders7Constants.resources, Integer> map : playerResources) {
             copy.playerResources.add(new HashMap<>(map));
         }
-        for (Deck<Wonder7Card> deck: playerHands) {
+        for (Deck<Wonder7Card> deck : playerHands) {
             copy.playerHands.add(deck.copy());
         }
-        for (Deck<Wonder7Card> deck: playedCards) {
+        for (Deck<Wonder7Card> deck : playedCards) {
             copy.playedCards.add(deck.copy());
         }
 
         copy.AgeDeck = AgeDeck.copy();
         copy.discardPile = discardPile.copy();
         copy.currentAge = currentAge;
-        copy.playerWonderBoard = playerWonderBoard.clone();
-        copy.turnActions = turnActions.clone();
 
+        for (int i = 0; i < getNPlayers(); i++) {
+            copy.playerWonderBoard[i] = playerWonderBoard[i].copy();
+        }
+        for (int i = 0; i < getNPlayers(); i++) {
+            if (turnActions[i] != null) copy.turnActions[i] = turnActions[i].copy();
+        }
         return copy;
     }
 
-    public void _reset(){
+    public void _reset() {
         // reset any variables that would have been changed 
         // (and not directly reset in the ForwardModel._setup() method) to their initial state
 
@@ -104,18 +111,18 @@ public class Wonders7GameState extends AbstractGameState {
                 Objects.equals(discardPile, that.discardPile) &&
                 Objects.equals(wonderBoardDeck, that.wonderBoardDeck) &&
                 Arrays.equals(playerWonderBoard, that.playerWonderBoard) &&
-                Arrays.equals(turnActions, that.turnActions)&&
+                Arrays.equals(turnActions, that.turnActions) &&
                 currentAge == that.currentAge;
 
     }
 
-    int _getGameScore(int playerId){
+    int _getGameScore(int playerId) {
         // return the players score for the current game state.
         // This may not apply for all games
         return playerId;
     }
 
-    public double _getHeuristicScore(int playerId){
+    public double _getHeuristicScore(int playerId) {
         // Implement a rough-and-ready heuristic (or a very sophisticated one) 
         // that gives an estimate of how well a player is doing in the range [-1, +1], 
         // where -1 is immediate loss, and +1 is immediate win
@@ -128,23 +135,58 @@ public class Wonders7GameState extends AbstractGameState {
     public double getGameScore(int playerId) {
         return 0;
     }
-    public int getCurrentAge(){return currentAge;}
 
-    public Deck<Wonder7Card> getAgeDeck(){return AgeDeck;}
+    public int getCurrentAge() {
+        return currentAge;
+    }
 
-    public Deck<Wonder7Card> getPlayerHand(int index){return playerHands.get(index);} // Get player hand
-    public List<Deck<Wonder7Card>> getPlayerHands(){return playerHands;}
+    public Deck<Wonder7Card> getAgeDeck() {
+        return AgeDeck;
+    }
 
-    public Deck<Wonder7Card> getPlayedCards(int index){return playedCards.get(index);} // Get player 'played' cards
+    public Deck<Wonder7Card> getPlayerHand(int index) {
+        return playerHands.get(index);
+    } // Get player hand
 
-    public Deck<Wonder7Card>  getDiscardPile(){return discardPile;}
-    public AbstractAction getTurnAction(int index){return turnActions[index];}
-    public void setTurnAction(int index, AbstractAction action){turnActions[index] = action;}
-    public Wonder7Board getPlayerWonderBoard(int index){return playerWonderBoard[index];}
-    public void setPlayerWonderBoard(int index, Wonder7Board wonder){playerWonderBoard[index] = wonder;}
+    public List<Deck<Wonder7Card>> getPlayerHands() {
+        return playerHands;
+    }
+
+    public Deck<Wonder7Card> getPlayedCards(int index) {
+        return playedCards.get(index);
+    } // Get player 'played' cards
+
+    public Deck<Wonder7Card> getDiscardPile() {
+        return discardPile;
+    }
+
+    public AbstractAction getTurnAction(int index) {
+        return turnActions[index];
+    }
+
+    public void setTurnAction(int index, AbstractAction action) {
+        turnActions[index] = action;
+    }
+
+    public Wonder7Board getPlayerWonderBoard(int index) {
+        return playerWonderBoard[index];
+    }
+
+    public void setPlayerWonderBoard(int index, Wonder7Board wonder) {
+        playerWonderBoard[index] = wonder;
+    }
 
 
-    public List<HashMap<Wonders7Constants.resources, Integer>> getAllPlayerResources(){return playerResources;} // Return all player's resources hashmap
+    public List<HashMap<Wonders7Constants.resources, Integer>> getAllPlayerResources() {
+        return playerResources;
+    } // Return all player's resources hashmap
 
-    public HashMap<Wonders7Constants.resources, Integer> getPlayerResources(int index){return playerResources.get(index);} // Return players resource hashmap
+    public HashMap<Wonders7Constants.resources, Integer> getPlayerResources(int index) {
+        return playerResources.get(index);
+    } // Return players resource hashmap
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), playerResources, playerHands, playedCards, AgeDeck, discardPile, wonderBoardDeck, playerWonderBoard, turnActions);
+    }
 }
