0deb166b887e3587958793f5a8ecaa16068f807d
awais <aryakakaroz@hotmail.com>
2023-05-19 20:10:07 +0100

    Wonders7GameParameters now extends tunableParameters. Removed unused parameters. Added reset() method.

--- src/main/java/games/wonders7/Wonders7GameParameters.java
+++ src/main/java/games/wonders7/Wonders7GameParameters.java
@@ -2,30 +2,40 @@ package games.wonders7;
 
 import core.AbstractGameState;
 import core.AbstractParameters;
+import core.Game;
+import evaluation.TunableParameters;
+import games.GameType;
 
-public class Wonders7GameParameters extends AbstractParameters {
+
+import java.util.Objects;
+
+public class Wonders7GameParameters extends TunableParameters {
     // The Parameters of the game should contain any Game constants.
     // Having them all in one place makes it easy to amend them, and also to tune them as part of Game Design.
     // For straightforward and simple examples, have a look at those for Diamant and DotsAndBoxes.
     //
 
-    public int nWonderBoards = 7;
     public int nWonderCardsPerPlayer = 7;
-    public int nAge1Cards = 49;
-    public int nAge2Cards, nAge3Cards = 50;
-    public int nConflictTokens = 46;
-    public int nValue3Coins = 24;
-    public int getnValue1Coins = 46;
+
 
 
 
     public Wonders7GameParameters(long seed) {
         super(seed);
+        addTunableParameter("nWonderCardsPerPlayer", 7);
+        _reset();
+    }
+
+    @Override
+    public void _reset() {
+        nWonderCardsPerPlayer = (int) getParameterValue("nWonderCardsPerPlayer");
     }
 
     @Override
     protected AbstractParameters _copy() {
-        return new Wonders7GameParameters(System.currentTimeMillis());
+        Wonders7GameParameters wgp = new Wonders7GameParameters(System.currentTimeMillis());
+        wgp.nWonderCardsPerPlayer = nWonderCardsPerPlayer;
+        return wgp;
     }
 
     @Override
@@ -35,5 +45,15 @@ public class Wonders7GameParameters extends AbstractParameters {
         if (!super.equals(o)) return false;
         Wonders7GameParameters that = (Wonders7GameParameters) o;
         return nWonderCardsPerPlayer == that.nWonderCardsPerPlayer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), nWonderCardsPerPlayer);
+    }
+
+    @Override
+    public Object instantiate() {
+        return new Game(GameType.Wonders7, new Wonders7ForwardModel(), new Wonders7GameState(this, GameType.Wonders7.getMinPlayers()));
     }
 }
